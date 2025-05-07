1866505717aa6bcbe22e03bc5589f3259b4a376f
Maxim Satanov <maxim.satanov@pairnote.com>
2025-05-07 21:47:09 +0500

    updated block creation

--- test.go
+++ test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-type Payment struct {
+type Transaction struct {
 	TrainerId   int             `json:"trainerId"`
 	ClientId    int             `json:"clientId"`
 	PaymentDate time.Time       `json:"paymentDate"`
@@ -18,12 +18,12 @@ type Payment struct {
 type Block struct {
 	Index       int
 	Hash        []byte
-	Transaction *Payment
+	Transaction *Transaction
 	PrevHash    []byte
 	Timestamp   time.Time
 }
 
-func createNewBlock(transaction *Payment, prevHash []byte, prevIndex int) *Block {
+func createNewBlock(transaction *Transaction, prevHash []byte, prevIndex int) *Block {
 	h := sha256.New()
 
 	transactionString := fmt.Sprintf("%d%d%s%s",
@@ -36,7 +36,7 @@ func createNewBlock(transaction *Payment, prevHash []byte, prevIndex int) *Block
 	h.Write([]byte(transactionString))
 	h.Write(prevHash)
 
-	block := &Block{
+	var block *Block = &Block{
 		Index:       prevIndex + 1,
 		Hash:        h.Sum(nil),
 		Transaction: transaction,
@@ -52,8 +52,8 @@ func main() {
 	hash.Write([]byte("genesis"))
 	previousHash := hash.Sum(nil)
 
-	payment := initTransaction()
-	block := createNewBlock(payment, previousHash, 0)
+	var initTransaction *Transaction = initTransaction()
+	var block *Block = createNewBlock(initTransaction, previousHash, 0)
 
 	fmt.Printf("Block Index: %d\n", block.Index)
 	fmt.Printf("Block Hash: %x\n", block.Hash)
@@ -61,8 +61,8 @@ func main() {
 	fmt.Printf("Transaction: %+v\n", block.Transaction)
 }
 
-func initTransaction() *Payment {
-	payment := &Payment{
+func initTransaction() *Transaction {
+	var payment *Transaction = &Transaction{
 		TrainerId:   1,
 		ClientId:    2,
 		PaymentDate: time.Now(),
